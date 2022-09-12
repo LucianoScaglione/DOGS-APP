82169741b3b9b72a13ae25b775285b2fe58e4711
LucianoScaglione <Lucianoscaglione21@gmail.com>
2022-09-12 15:55:25 -0300

    changes

--- client/src/components/Detail.jsx
+++ client/src/components/Detail.jsx
@@ -9,6 +9,7 @@ import { isAuthenticated } from './AuthService'
 const Detail = (props) => {
   const userId = isAuthenticated()
   const [comment, setComment] = useState('')
+  const [seeMore, setSeeMore] = useState(false)
   const comments = useSelector(state => state.comments)
   console.log("comments: ", comments)
   const id = props.match.params.id
@@ -31,7 +32,7 @@ const Detail = (props) => {
     <div className={style.detail}>
       <div className={style.detailContainer}>
         <Link to='/home'>
-          <p className={style.detailCategory}>Volver</p>
+          <p className={style.back}>Back</p>
         </Link>
         <div className={style.detailBody}>
           <img className={style.detailImg} src={detailDog.image} alt='Not found' />
@@ -46,20 +47,23 @@ const Detail = (props) => {
           </div>
         </div>
       </div>
-      <div className={style.containerLower}>
-        <h3 className={style.detailh3}>We would like to know what you think of this breed!</h3>
-        <textarea className={style.detailTextArea} placeholder='Add your comment' maxLength={140} onChange={(e) => setComment(e.target.value)} /> <br />
-        <button onClick={handleSubmit}>Send</button>
-        <h4 className={style.detailh4}>Comments:</h4>
-        {comments ? comments.map(c => {
-          return (
-            <div className={style.detailInfoUser} value={c.DogId}>
-              <p>{c.User.fullname} says:</p>
-              <p>{c.comment}</p>
-            </div>
-          )
-        }) : <p>Without comments</p>}
-      </div>
+      {!seeMore ? <button className={style.seeMore} onClick={() => setSeeMore(true)}>See more</button> : <button className={style.close} onClick={() => setSeeMore(false)}>Close</button>}
+      {seeMore &&
+        <div className={style.containerLower}>
+          <h3 className={style.detailh3}>We would like to know what you think of this breed!</h3>
+          <textarea className={style.detailTextArea} placeholder='Add your comment' maxLength={140} onChange={(e) => setComment(e.target.value)} /> <br />
+          <button onClick={handleSubmit}>Send</button>
+          <h4 className={style.detailh4}>Comments:</h4>
+          {comments ? comments.map(c => {
+            return (
+              <div className={style.detailInfoUser} value={c.DogId}>
+                <p>{c.User.fullname} says:</p>
+                <p>{c.comment}</p>
+              </div>
+            )
+          }) : <p>Without comments</p>}
+        </div>
+      }
     </div>
   )
 }
